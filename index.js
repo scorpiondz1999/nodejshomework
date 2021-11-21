02544471dad0efc885ce923c41e0f5142ce52a21
Djamel Ramdani <enzonpapa@gmail.com>
2021-11-21 14:36:48 -0600

    setup questions

--- index.js
+++ index.js
@@ -1,7 +1,125 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ {
+        type: 'input',
+        message: "What is your GitHub username? ",
+        name: 'username',
+        
+        next_question: function (answer) {
+            if (answer.length === 0) {
+                console.log("A valid GitHub username is required !");
+                return false;
+            };
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name email Address?",
+        name: 'repo',
+        
+        next_question: function (answer) {
+            if (answer.length === 0) {
+                console.log("A valid name email Address is required !");
+                return false;
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your project name?",
+        name: 'title',
+        
+        next_question: function (answer) {
+            if (answer.length === 0) {
+                console.log("A valid project name is required !");
+                return false;
+            }
+            return true;
+        }
+    },
+    
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        
+        next_question: function (answer) {
+            if (answer.length === 0) {
+                console.log("A valid project description is required !");
+                return false;
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "command should to be run to install depedencies?",
+        name: 'repo',
+        
+        next_question: function (answer) {
+            if (answer.length === 0) {
+                console.log("A valid command is required !");
+                return false;
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What does the user need to know about using the repo?",
+        name: 'repo',
+        
+        next_question: function (answer) {
+            if (answer.length === 0) {
+                console.log("A valid answer is required !");
+                return false;
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "what does the user need to know about constributing to the repo?",
+        name: 'repo',
+        
+        next_question: function (answer) {
+            if (answer.length === 0) {
+                console.log("A valid answer is required !");
+                return false;
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for the project.",
+        choices: ['MIT', 'Apache 2.0', 'GPL'],
+        name: 'license'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
